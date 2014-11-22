f5b0b2c72febba212ed05e3baabd26ed7613cc43
Radoslaw Stolarczyk <trythin@gmail.com>
2014-11-22 10:00:05 +0100

    Whole commit
    Upgrade main with error dsafds
    
    git ignore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
+#include "multiple.h"
+#include "add.h"
+
 int main()
 {
-
-
+	std::cout << multiple(2,add(2,5)) << std::endl;
 	return 0;
 }
---
+++ multiple.h
@@ -0,0 +1,7 @@
+#ifndef MULTIPLE_H
+#define MULTIPLE_H
+int multiple(const int a, const int b)
+{
+	return a*b;
+}
+#endif
